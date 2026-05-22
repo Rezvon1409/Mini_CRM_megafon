efab42a5717d1ac3cc3bf43944f8cd4f7b1df50c
Rezvon1409 <rezvon0880@gmail.com>
2026-05-22 11:14:48 +0500

    implement access and refresh token logic for professional security

--- schemas.py
+++ schemas.py
@@ -192,7 +192,10 @@ class LoginRequest(BaseModel):
 
 class Token(BaseModel):
     access_token: str
+    refresh_token : str
     token_type: str = 'bearer'
+
+    model_config = ConfigDict(from_attributes=True)
     
 class TokenData(BaseModel):
     user_id: Optional[int] = None
